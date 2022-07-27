fc25627138fa149aeb77106831beb9612f94033e
DariaIsaenko <dariashkvarkova@gmail.com>
2022-07-28 01:46:17 +0300

    Исправлено меню, фоны. Элементам добавлены состояния.

--- source/js/main.js
+++ source/js/main.js
@@ -1,40 +1,49 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const body = document.querySelector('.page__body');
+const advantagesLink = document.querySelector('.main-nav__link--advantages');
+const catalogLink = document.querySelector('.main-nav__link--catalog');
+const contactsLink = document.querySelector('.main-nav__link--contacts');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('is-nojs');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+function getBodyScrollTop() {
+  return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
+}
+
+navToggle.addEventListener('click', () => {
+
+  if (navMain.classList.contains('is-closed')) {
+    body.dataset.scrollY = getBodyScrollTop();
+    body.style.top = `-${body.dataset.scrollY}px`;
+    navMain.classList.remove('is-closed');
+    navMain.classList.add('is-opened');
+    body.classList.add('is-locked');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('is-closed');
+    navMain.classList.remove('is-opened');
+    body.classList.remove('is-locked');
+    window.scrollTo(0, body.dataset.scrollY);
   }
 });
 
+function getSmoothScrolling(element) {
+  window.scroll({
+    left: 0,
+    top: element.offsetTop,
+    behavior: 'smooth',
+  });
+}
 
-// ---------------------------------
+function getLinkMobile(element) {
+  if (navMain.classList.contains('is-opened')) {
+    navMain.classList.add('is-closed');
+    navMain.classList.remove('is-opened');
+    body.classList.remove('is-locked');
+    getSmoothScrolling(document.getElementById(element));
+  }
+}
 
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
+advantagesLink.addEventListener('click', () => getLinkMobile('advantages'));
+catalogLink.addEventListener('click', () => getLinkMobile('catalog'));
+contactsLink.addEventListener('click', () => getLinkMobile('contacts'));
